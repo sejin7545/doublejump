cd9049db35aed93387e5090a3c93e6e6e720be2d
sejin7545 <wjdtpwlsrnt3@naver.com>
2020-12-03 19:22:54 +0900

    기초작업 work1

---
+++ Assets/GoalArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalArea : MonoBehaviour
+{
+    public static bool goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = false;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            goal = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
