fd8173bea2e763b64c52e4a1e367bea9d4e09e29
PolarisS2 <cktjdqls1234@naver.com>
2020-12-03 20:56:00 +0900

    work2 / 배경/카메라/캐릭터변경

---
+++ Assets/GameResult.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameResult : MonoBehaviour
+{
+    private int highScore;
+    public Text resultTime;
+    public Text bestTime;
+    public GameObject parts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+        }
+        else
+        {
+            highScore = 999;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GoalArea.goal)
+        {
+            parts.SetActive(true);
+            int result = Mathf.FloorToInt(Timer.time);
+            resultTime.text = "ResultTime" + result;
+            bestTime.text = "BestTime" + highScore;
+
+            if (highScore > result)
+            {
+                PlayerPrefs.SetInt("HighScoer", result);
+
+            }
+        }
+    }
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Assets/Out.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
